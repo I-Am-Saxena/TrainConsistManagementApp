6adb2687890ae05cf3ea6c323b5274613eef259e
I-Am-Saxena <amolsaxena.personal@gmail.com>
2026-04-08 14:30:08 +0530

    UC17 Arrays Sort

---
+++ src/UC17ArraysSort.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class UC17ArraysSort {
+    public static void main(String[] args) {
+        String[] names={"Z","A","M"};
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));
+    }
+}
